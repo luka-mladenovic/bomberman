b25f95f4720a2f0fd57f4bb4a1227d3cd87702a9
luka-mladenovic <luka@kreativika.si>
2019-04-01 19:16:07 +0200

    Add object rotator script

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script takes handling of object rotation  
+ */
+public class Rotator : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
